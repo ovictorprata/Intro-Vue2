855ffd95b444a4dbfa11ce451c1b1e672fd8e713
Roger Camargo <huogerac@gmail.com>
2022-06-04 14:46:09 -0300

    Indicador de curso ativo/não ativo

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+  el: "#app",
+  data: {
+    course: "D-Já Vue - Uma jornada pela Web",
+    image: "./images/dejavu_matrix.jpg",
+    description: "Como construir aplicações web com agilidade e qualidade.",
+    duration: 42,
+    ativo: false,
+  },
+});
